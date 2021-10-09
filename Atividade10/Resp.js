e583aeefcd423c7f2024e85d4649e92af698397d
JesusPorfirio <61605776+JesusPorfirio@users.noreply.github.com>
2021-10-08 21:34:20 -0300

    Add files via upload

--- Atividade10/Resp.js
+++ Atividade10/Resp.js
@@ -1,9 +1,44 @@
-function Retangulo(base, altura) {
-    this.base = base;
-    this.altura = altura;
-
-    return parseFloat(base) * parseFloat(altura);
+function clicar(){
+    let aluno=[];
+    for(let x =0; x<3; x++){
+        aluno.push(window.prompt("Digite um nome:"));
+        aluno.push(window.prompt("Digite um Ra:"));
+    }
+    for(let x =0; x<aluno.length; x++){
+        window.alert(aluno[x])
+    }
 }
-const retangulo1 = new Retangulo(2, 4);
+function clicar1(){
+    let aluno=[];
+    let ra=[];
+    for(let x =0; x<3; x++){
+        aluno.push(window.prompt("Digite um nome:"));
+        ra.push(window.prompt("Digite um Ra:"));
+    }
+    for(let x =0; x<aluno.length; x++){
+        window.alert(aluno[x] + ra[x]);
+    }
+}
+function clicar2(){
+    let aluno1, aluno2,aluno3;
+    let ra1,ra2,ra3;
+        aluno1=(window.prompt("Digite um nome:"));
+        ra1=(window.prompt("Digite um Ra:"));
+        
+        aluno2=(window.prompt("Digite um nome:"));
+        ra2=(window.prompt("Digite um Ra:"));
 
-console.log(Retangulo1);
+        aluno3=(window.prompt("Digite um nome:"));
+        ra3=(window.prompt("Digite um Ra:"));
+
+
+        window.alert(aluno1);
+        window.alert(ra1);
+
+        window.alert(aluno2);
+        window.alert(ra2);
+        
+        window.alert(aluno3);
+        window.alert(ra3);
+
+}
